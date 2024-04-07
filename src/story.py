b8710803428fe9bd0cdbfb4e17b53873818c75cb
Eric Wang <eric@titanium.agency>
2024-04-07 03:45:17 -0400

    searchuser route

--- src/story.py
+++ src/story.py
@@ -12,11 +12,33 @@ def getStory():
         title = request.args.get('title')
         id = request.args.get('id')
         print(id)
+        print(title)
         query += ' WHERE'
-        if (len(id)):
+        if (id):
             query += (' s.id in ('+id+')')
+        elif (title):
+            query += (' LOWER(title) LIKE \'%'+title+'%\'')
+        print(query)
     stories = db.execute(query).fetchall()
     out = []
     for row in stories:
+        out.append(list(row))
+    return out
+
+@bp.route('/user')
+def getUser():
+    db = get_db()
+    print(request.query_string)
+    query = 'SELECT id, name, email, username, about, location FROM user'
+    if (len(request.query_string)):
+        username = request.args.get('query')
+        print(username)
+        query += ' WHERE'
+        if (username):
+            query += (' LOWER(name) LIKE \'%'+username+'%\'')
+            query += (' OR LOWER(username) LIKE \'%'+username+'%\'')
+    users = db.execute(query).fetchall()
+    out = []
+    for row in users:
         out.append(list(row))
     return out
